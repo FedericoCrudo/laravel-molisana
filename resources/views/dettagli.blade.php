e88f6c2ed4fb8583e0fb1783578dc429f15e099d
Federico <crudofederico@gmail.com>
2021-03-11 18:13:13 +0100

    complete

--- resources/views/dettagli.blade.php
+++ resources/views/dettagli.blade.php
@@ -1,10 +1,34 @@
 @extends('layout.app')
 
+@section('fontaw')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+@endsection
 
 @section('content')
     <div class="container">
         <div class="info-prodotto">
-            @dd($prodotto);
+           <h2>{{$prodotto['titolo']}}</h2>
+           <div class="img-prodotto">
+                <img src="{{$prodotto['src-h']}}" alt="">
+           </div>
+            <div class="description">
+                <div class="desc">{!!$prodotto['descrizione']!!}</div>
+                <div class="section-box">
+                    <div class="box-info">
+                        <i class="fas fa-info"></i>
+                        <span>{{$prodotto['tipo']}}</span>
+                    </div>
+                    <div class="box-info">
+                        <i class="far fa-clock"></i>
+                        <span>{{$prodotto['cottura']}}</span>
+                    </div>
+                    <div class="box-info">
+                        <i class="fas fa-balance-scale-right"></i>
+                        <span>{{$prodotto['peso']}}</span>
+                    </div>
+                </div>
+                
+            </div>
         </div>
     </div>
 
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,9 @@
     </div>
     <div class="menu">
         <ul>
-        <li><a href="{{route ('home-route')}}">Home</a></li>
-        <li><a href="{{route ('product-route')}}">Product</a></li>
-        <li><a href="{{route ('news-route')}}">News</a></li>
+        <li><a href="{{route ('home-route')}}" class="{{(Request::route()->getName()=='home-route')? 'active' :''}}">Home</a></li>
+        <li><a href="{{route ('product-route')}}"class="{{(Request::route()->getName()=='product-route')? 'active' :''}}">Product</a></li>
+        <li><a href="{{route ('news-route')}}"class="{{(Request::route()->getName()=='news-route') ? 'active' :''}}">News</a></li>
         
         
         
